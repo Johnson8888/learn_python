2e6f6a3db9aa7067d810a775e06f54da2d853424
JaimeCerseiLannister <zhx1222@foxmail.com>
2020-11-01 14:11:42 +0800

    1-1 & 1-2

---
+++ 1-1/simple_message.py
@@ -0,0 +1,14 @@
+'''
+Author: 弗拉德
+Date: 2020-11-01 13:56:26
+LastEditTime: 2020-11-01 14:00:57
+Support: http://fulade.me
+'''
+
+
+# 作业链接：http://fulade.me/python-variable-1-5.html
+
+# 1-1 简单消息：将一条消息存储到变量中，再将其打印出来。
+
+message = "my blog is fulade.me"
+print(message)
---
+++ 1-2/simple_messages.py
@@ -0,0 +1,18 @@
+'''
+Author: 弗拉德
+Date: 2020-11-01 14:01:22
+LastEditTime: 2020-11-01 14:03:12
+Support: http://fulade.me
+'''
+
+
+# 作业链接：http://fulade.me/python-variable-1-5.html
+
+# 1-2 多条简单消息：将一条消息存储到变量中，将其打印出来；再将变量的值修改
+
+
+message = "my blog is fulade.me"
+print(message)
+
+message = "have fun!"
+print(message)
