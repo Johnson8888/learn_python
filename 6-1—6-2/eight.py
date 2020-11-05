304df5f0d2495e4270dfb875f091be4c8d777728
Johnson8888 <zhx1222@foxmail.com>
2020-11-05 20:06:13 +0800

    6-1

---
+++ 6-1—6-2/eight.py
@@ -0,0 +1,20 @@
+'''
+Author: 弗拉德
+Date: 2020-11-05 19:57:01
+LastEditTime: 2020-11-05 20:01:55
+Support: http://fulade.me
+'''
+
+# 题目地址：http://fulade.me/python-number-1-6.html
+
+
+# 6-1 编写 4 个表达式，它们分别使用加法、减法、乘法和除法运算，但结果都是数字 `8`。
+# 为使用 print 语句来显示结果，务必将这些表达式用括号括起来，也就是说，你应该编写4行类似于下面的代码:
+# `print(5 + 3)`
+# 输出应为 4 行，其中每行都只包含数字8。命名为`eight.py`
+
+
+print(1 + 7)
+print(16 - 8)
+print(2 * 4)
+print(int(24 / 3))
---
+++ 6-1—6-2/number.py
@@ -0,0 +1,16 @@
+'''
+Author: 弗拉德
+Date: 2020-11-05 20:01:22
+LastEditTime: 2020-11-05 20:05:51
+Support: http://fulade.me
+'''
+
+
+# 题目地址：http://fulade.me/python-number-1-6.html
+
+
+# 6-2 将你最喜欢的数字存储在一个变量中，再使用这个变量创建一条消息，
+# 指出你最喜欢的数字，然后将这条消息打印出来。命名为`number.py`
+
+favorite_number = 7
+print("My favorite number is " + str(favorite_number) + " !")
