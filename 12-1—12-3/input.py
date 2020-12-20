edea9e619654f710b71963044451892a56ead339
JaimeCerseiLannister <zhx1222@foxmail.com>
2020-12-20 15:36:46 +0800

    12.1

---
+++ 12-1—12-3/input.py
@@ -0,0 +1,19 @@
+'''
+Author: 弗拉德
+Date: 2020-12-20 15:28:23
+LastEditTime: 2020-12-20 15:31:53
+Support: http://fulade.me
+'''
+
+
+### 题目地址： http://fulade.me/python-input-12.html
+
+if __name__ == "__main__":
+    
+
+    #12.1 编写一个程序，询问用户要咨询什么样的汽车，并打印一条消息，如"Let me see if I can find you a Subaru"。
+    car_name = input("Tell me a name of the car:")
+    print("Let me see if I can find you a " + car_name)  
+    
+    #12.2 餐馆订位：编写一个程序，询问用户有多少人用餐。如果超过 8 人，就打印一条消息，指出没有空桌；否则指出有空桌。
+    #12.3 让用户输入一个数字，并指出这个数字是否是 10 的整数倍。
